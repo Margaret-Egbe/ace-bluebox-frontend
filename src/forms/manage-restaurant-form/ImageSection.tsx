a7c5b5dfc9ba51b40e2bc4735b39df2b338342a7
Margaret Egbe <marymargaret168@gmail.com>
2024-06-20 19:26:42 +0100

    updated image features

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
+/*import { AspectRatio } from "@/components/ui/aspect-ratio";
 import {
   FormControl,
   FormDescription,
@@ -61,3 +61,78 @@ const ImageSection = () => {
 };
 
 export default ImageSection;
+*/
+
+
+
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+
+const ImageSection = () => {
+  const { control, watch } = useFormContext();
+
+  const existingImageUrl = watch("imageUrl");
+
+  // Ensure the URL uses HTTPS
+  const secureImageUrl = existingImageUrl
+    ? existingImageUrl.startsWith("http:")
+      ? existingImageUrl.replace("http:", "https:")
+      : existingImageUrl
+    : null;
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-bold">Image</h2>
+        <FormDescription>
+          Add an image that will be displayed on your restaurant listing in the
+          search results. Adding a new image will overwrite the existing one.
+        </FormDescription>
+      </div>
+  
+      <div className="flex flex-col gap-8 md:w-[50%]">
+        {secureImageUrl && (
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src={secureImageUrl}
+              className="rounded-md object-cover h-full w-full"
+              alt="Restaurant"
+            />
+          </AspectRatio>
+        )}
+        <FormField
+          control={control}
+          name="imageFile"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="bg-[#eee]"
+                  type="file"
+                  accept=".jpg, .jpeg, .png"
+                  onChange={(event) =>
+                    field.onChange(
+                      event.target.files ? event.target.files[0] : null
+                    )
+                  }
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSection;
+
