20b937fbeef718c41af636a8d2b157c5f0f141a4
Margaret-Egbe <marymargaret168@gmail.com>
2024-07-28 13:45:51 -0700

    added a checkout flow

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -4,9 +4,12 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { useParams } from "react-router-dom";
 import MenuItem from "../components/MenuItem";
 import { useState } from "react";
-import { Card } from "@/components/ui/card";
+import { Card, CardFooter } from "@/components/ui/card";
 import OrderSummary from "@/components/OrderSummary";
 import { MenuItem  as MenuItemTypes} from "@/types";
+import CheckoutButton from "@/components/CheckoutButton";
+import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 
 export type CartItem = {
@@ -19,8 +22,13 @@ export type CartItem = {
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const { createCheckoutSession, isLoading: isCheckoutLoading } =
+    useCreateCheckoutSession();
 
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
 
   const addToCart = (menuItem: MenuItemTypes) => {
     setCartItems((prevCartItems) => {
@@ -49,8 +57,13 @@ const DetailPage = () => {
       ];
     }
 
+    sessionStorage.setItem(
+      `cartItems-${restaurantId}`,
+      JSON.stringify(updatedCartItems)
+    );
+
     return updatedCartItems
-    })
+    });
   };
 
   const removeFromCart =  (cartItem: CartItem) => {
@@ -59,9 +72,39 @@ const DetailPage = () => {
         (item) => cartItem._id !== item._id
       );
 
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+
       return updatedCartItems;
-    })
-  }
+    });
+  };
+
+  const onCheckout = async (userFormData: UserFormData) => {
+    if (!restaurant) {
+      return;
+    }
+
+    const checkoutData = {
+      cartItems: cartItems.map((cartItem) => ({
+        menuItemId: cartItem._id,
+        name: cartItem.name,
+        quantity: cartItem.quantity.toString(),
+      })),
+      restaurantId: restaurant._id,
+      deliveryDetails: {
+        name: userFormData.name,
+        addressLine1: userFormData.addressLine1,
+        city: userFormData.city,
+        country: userFormData.country,
+        email: userFormData.email as string,
+      },
+    };
+
+    const data = await createCheckoutSession(checkoutData);
+    window.location.href = data.url;
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -91,6 +134,13 @@ const DetailPage = () => {
              cartItems={cartItems} 
              removeFromCart={removeFromCart}
             />
+            <CardFooter>
+              <CheckoutButton
+               disabled={cartItems.length === 0}
+               onCheckout={onCheckout}
+               isLoading={isCheckoutLoading}
+              />
+            </CardFooter>
           </Card>
          </div>
        </div>
